16b1c2526495027fee479f96c684edb1d2de890e
TobiOladoja <tbfullstack@gmail.com>
2020-01-02 22:26:13 +0000

    Added correct fontawesome CDN to index.html then added some icons to TodoItem for trash and edit icon

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -6,6 +6,14 @@ export default class TodoItem extends Component {
     return (
       <li className='list list-group-item capitalize d-flex justify-content-between my-3'>
         <h6>{title}</h6>
+        <div className='todo-icon'>
+          <span className='mx-2 text-success'>
+            <i className='fas fa-pen'></i>
+          </span>
+          <span className='mx-2 text-danger'>
+            <i className='fas fa-trash'></i>
+          </span>
+        </div>
       </li>
     );
   }
