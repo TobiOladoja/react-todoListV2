5c02ccaff25dc1ecee114e038b9056567115729f
TobiOladoja <tbfullstack@gmail.com>
2020-01-02 20:07:29 +0000

    Testing handleSubmit method on app js by adding console log and linking it to the submit bottom TodoInput component

--- src/App.js
+++ src/App.js
@@ -6,13 +6,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import uuid from 'uuid';
 
 class App extends Component {
+  state = {
+    items: [],
+    id: 0,
+    item: '',
+    editItem: false
+  };
+
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    console.log('You clicked submit');
+    // To prevent from refreshing the page.
+  };
   render() {
     return (
       <div className='container'>
         <div className='row'>
           <div className='col-10 mx-auto col-md-8 mt-4'>
             <h3 className='text-capitalize text-center'>Todo Input</h3>
-            <ToDoInput />
+            {/* Passed both item and handleChange to TodoInput component */}
+            <ToDoInput
+              item={this.state.item}
+              handleChange={this.handleChange}
+              handleSubmit={this.handleSubmit}
+            />
             <TodoList />
           </div>
         </div>
@@ -27,3 +50,5 @@ export default App;
 // TodoInput component Hold form and item > importing to app.js. Because app js wil be rendering ToDoInput.
 // TodoList  component Hold form and item > importing to app.js Because app js wil lbe rendering ToDoList.
 // TodoItem component for each and every to do > importing to TodoList.js Because ToDoList will be rendering ToDoItem
+
+//
--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -2,17 +2,19 @@ import React, { Component } from 'react';
 
 export default class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit } = this.props; //from App.js the state item and method/function want to use.
     return (
       <div className='card card-body my-3'>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='input-group'>
-            <div className='input-group-prepend'>
-              <div className='input-group-text bg-primary text-white'></div>
-            </div>
+            <div className='input-group-prepend'></div>
             <input
               type='text'
               className='form-control'
               placeholder='add a todo item'
+              /* from const item and handleChange, declared on top of here but passed from app.js */
+              value={item}
+              onChange={handleChange}
             />
           </div>
           <button type='submit' className='btn btn-block btn-primary mt-3'>
